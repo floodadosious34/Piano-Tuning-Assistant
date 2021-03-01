19394e9550bebd72c2c4828948ade1287fa370ff
James Flood <floodadosious111@gmail.com>
2021-02-28 22:31:38 -0500

    added intial function

--- piano_assistant/piano_assistan_app.py
+++ piano_assistant/piano_assistan_app.py
@@ -1,3 +1,14 @@
 def get_piano_notes():
     octave = ['C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g', 'A', 'a', 'B']
     
+    
+try:
+    note1 = input("Pick a root note from A-G ")
+    note2 = input("Pick a note you want to test against the root note. ")
+except ValueError:
+    print("That is not a valid note. Please try again")
+else:
+    print("You chose {} and {}, is this correct?".format(note1, note2))
+
+get_piano_notes()
+
