250074ec332166c40923af49a19ae2a96327d687
James Flood <floodadosious111@gmail.com>
2021-03-08 17:42:48 -0500

    added master loop

--- piano_assistant/piano_assistan_app.py
+++ piano_assistant/piano_assistan_app.py
@@ -1,14 +1,82 @@
+import sys
+import time
+
+
+notes_ocatve = (
+    ("C4", 1),
+    ("Cs4", 2),
+    ("Df4", 2),
+    ("D4", 3),
+    ("Ds4", 4),
+    ("Ef4", 4),
+    ("E4", 5),
+    ("F4", 6),
+    ("Fs4", 7),
+    ("Gf4", 7),
+    ("G4", 8),
+    ("Gs4", 9),
+    ("Af4", 9),
+    ("A4", 10),
+    ("As4", 11),
+    ("Bf4", 11),
+    ("B4", 12),
+    ("C5", 13),
+)
+
+
+selected_notes = []
+
+def add_notes(notes):
+    selected_notes.append(notes)
+    print("You added {}".format(notes))
+
 def get_piano_notes():
-    octave = ['C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g', 'A', 'a', 'B']
-    
-    
-try:
-    note1 = input("Pick a root note from A-G ")
-    note2 = input("Pick a note you want to test against the root note. ")
-except ValueError:
-    print("That is not a valid note. Please try again")
-else:
-    print("You chose {} and {}, is this correct?".format(note1, note2))
+    print("""
 
+
+
+    >>>>>>> Welcome to the Piano Tuning Assistant! <<<<<<<
+    >>>>>>> Enter 'QUIT' to exit the program. <<<<<<<<<<<<
+    >>>>>>> Enter 'DISPLAY' to show selected notes <<<<<<<
+
+
+
+
+    """)
+
+def display_notes():
+    print("Here are the notes you chose.")
+    for note in selected_notes:
+        print(note)
+
+def find_interval(arg, arg2):
+        for note in notes_ocatve:
+            if str(arg) == note[0]:
+                number1 = note[1]
+            if str(arg2) == note[0]:
+                number2 = note[1]
+                total = number2 - number1
+                time.sleep(0.9)
+        print("That is {} half steps".format(total))
+
+            
 get_piano_notes()
+while True:
+    note1 = input("Enter a root test note (e.g. C4, Cs4/Df4; s=sharp, f=flat). ")
+    if note1 == 'QUIT':
+        break
+    elif note1 == 'DISPLAY':
+        display_notes()
+        continue
+    
 
+    add_notes(note1)
+
+    note2 = input("Enter a second test note. ")
+    if note2 == 'QUIT':
+        break
+    
+
+    add_notes(note2)
+    display_notes()
+    find_interval(note1, note2)
