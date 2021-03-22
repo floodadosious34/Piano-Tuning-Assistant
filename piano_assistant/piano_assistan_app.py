9bd22a96e2d9b3072502a19dbe353c817eb5bdbb
James Flood <floodadosious111@gmail.com>
2021-03-22 17:52:31 -0400

    added else statement to catch notes that are beyond testing

--- piano_assistant/piano_assistan_app.py
+++ piano_assistant/piano_assistan_app.py
@@ -46,45 +46,135 @@ def main():
         quit
 
 
+# Tuple for listing out notes in two octaves
 notes_ocatve = [
-    ("C4", 1),
-    ("Cs4", 2),
-    ("Df4", 2),
-    ("D4", 3),
-    ("Ds4", 4),
-    ("Ef4", 4),
-    ("E4", 5),
-    ("F4", 6),
-    ("Fs4", 7),
-    ("Gf4", 7),
-    ('G4', 8),
-    ("Gs4", 9),
-    ("Af4", 9),
-    ("A4", 10),
-    ("As4", 11),
-    ("Bf4", 11),
-    ("B4", 12),
-    ("C5", 13),
-    ("Cs5", 14),
-    ("Df5", 14),
-    ("D5", 15),
-    ("Ds5", 16),
-    ("Ef5", 16),
-    ("E5", 17),
-    ("F5", 18),
-    ("Fs5", 19),
-    ("Gf5", 19),
-    ("G5", 20),
-    ("Gs5", 21),
-    ("Af5", 21),
-    ("A5", 22),
-    ("As5", 23),
-    ("Bf5", 23),
-    ("B5", 24),
-    ("C6", 25),
-    ("Cs6", 26)
+    ("A0", 1),
+    ("As0", 2),
+    ("Bf0", 2),
+    ("B0", 3),
+    ("C1", 4),
+    ("Cs1", 5),
+    ("Df1", 5),
+    ("D1", 6),
+    ("Ds1", 7),
+    ("Ef1", 7),
+    ("E1", 8),
+    ("F1", 9),
+    ("Fs1", 10),
+    ("Gf1", 10),
+    ('G1', 11),
+    ("Gs1", 12),
+    ("Af1", 12),
+    ("A1", 13),
+    ("As1", 14),
+    ("Bf1", 14),
+    ("B1", 15),
+    ("C2", 17),
+    ("Cs2", 18),
+    ("Df2", 18),
+    ("D2", 19),
+    ("Ds2", 20),
+    ("Ef2", 20),
+    ("E2", 21),
+    ("F2", 22),
+    ("Fs2", 23),
+    ("Gf2", 23),
+    ("G2", 24),
+    ("Gs2", 25),
+    ("Af2", 25),
+    ("A2", 26),
+    ("As2", 27),
+    ("Bf2", 27),
+    ("B2", 28),
+    ("C3", 29),
+    ("Cs3", 30),
+    ("Df3", 30),
+    ("D3", 31),
+    ("Ds3", 32),
+    ("Ef3", 32),
+    ("E3", 33),
+    ("F3", 34),
+    ("Fs3", 35),
+    ("Gf3", 35),
+    ('G3', 36),
+    ("Gs3", 37),
+    ("Af3", 37),
+    ("A3", 38),
+    ("As3", 39),
+    ("Bf3", 39),
+    ("B3", 40),
+    ("C4", 41),
+    ("Cs4", 42),
+    ("Df4", 42),
+    ("D4", 43),
+    ("Ds4", 44),
+    ("Ef4", 44),
+    ("E4", 45),
+    ("F4", 46),
+    ("Fs4", 47),
+    ("Gf4", 47),
+    ('G4', 48),
+    ("Gs4", 49),
+    ("Af4", 49),
+    ("A4", 50),
+    ("As4", 51),
+    ("Bf4", 51),
+    ("B4", 52),
+    ("C5", 53),
+    ("Cs5", 54),
+    ("Df5", 54),
+    ("D5", 55),
+    ("Ds5", 56),
+    ("Ef5", 56),
+    ("E5", 57),
+    ("F5", 58),
+    ("Fs5", 59),
+    ("Gf5", 59),
+    ("G5", 60),
+    ("Gs5", 61),
+    ("Af5", 61),
+    ("A5", 62),
+    ("As5", 63),
+    ("Bf5", 63),
+    ("B5", 64),
+    ("C6", 65),
+    ("Cs6", 66),
+    ("D6", 67),
+    ("Ds6", 68),
+    ("Ef6", 69),
+    ("E6", 70),
+    ("F6", 71),
+    ("Fs6", 72),
+    ("Gf6", 72),
+    ('G6', 73),
+    ("Gs6", 74),
+    ("Af6", 74),
+    ("A6", 75),
+    ("As6", 76),
+    ("Bf6", 76),
+    ("B6", 77),
+    ("C7", 78),
+    ("Cs7", 79),
+    ("Df7", 79),
+    ("D7", 80),
+    ("Ds7", 81),
+    ("Ef7", 81),
+    ("E7", 82),
+    ("F7", 83),
+    ("Fs7", 84),
+    ("Gf7", 84),
+    ("G7", 85),
+    ("Gs7", 86),
+    ("Af7", 86),
+    ("A7", 87),
+    ("As7", 88),
+    ("Bf7", 88),
+    ("B7", 89),
+    ("C8", 90)
 ]
 
+
+# Tuple for listing all intervals and coincident partials
 interval_name = (
     ("1st", 1, "n/a"),
     ("2nd", 2, "n/a"),
@@ -117,13 +207,16 @@ interval_name = (
 )
 
 
+# List that populates with selectd notes
 selected_notes = []
 
+# Adds selected notes to selected_notes list
 def add_notes(arg):
     note = arg
     selected_notes.append(note)
   
 
+# Starts the program
 def get_piano_notes():
     print("""
 
@@ -138,6 +231,8 @@ def get_piano_notes():
 
     """)
 
+
+# Displays selected notes on command
 def display_notes():
     answer = input("Would you like to see the notes you chose? y/n")
     if answer == 'y':
@@ -145,8 +240,9 @@ def display_notes():
             print(note)
     elif answer == 'n':
         quit
-        
 
+
+# Finds interval between the two selected notes
 def find_interval(arg, arg2):
     for note in notes_ocatve:
         if str(arg) == note[0]:
@@ -158,27 +254,28 @@ def find_interval(arg, arg2):
                     total = number2 - number1
                     time.sleep(0.9)
                 print("That is {} half steps".format(total))
-            except UnboundLocalError:
+            except UnboundLocalError: #checks if the notes is in the wrong order and gives a -x result
                 print("That is not a valid entry. Make sure the root note is smaller than the second note")
                 return False
                 
             
-
-    while True:
+    # Looks for interval name based on result from previous loop
+    while total <= 28:
         for interval in interval_name:
             if total == interval[1]:
                 total = interval[0]
                 total1 = interval[2]
-                print("The interval between {} and {} is a {}".format(arg, arg2, total))
-                
+                print("The interval between {} and {} is a {}".format(arg, arg2, total))                    
                 answer = input("Would you like to know the coincidental partial? (y/n)")
                 if answer == 'y':
                     print("The coincidental partial is {}".format(total1))
                     return False
                 elif answer == 'n':
                     return False
-        
+    else:
+        print("Those notes cannot be tested")
 
+# Test to see if the inputed notes are actually in the list of predetermined notes
 def test_note(arg):
     testN = str(arg)
     index = 1
@@ -193,8 +290,9 @@ def test_note(arg):
     return False
         
 
-            
+# Call to start program            
 get_piano_notes()
+# Master loop for the program
 while True:
     run = input("Hello, if you would like to start press r. If you would like to quit, type q.")
     if run == 'q':
@@ -228,11 +326,7 @@ while True:
             if intervalStatus is False:
                 break
         display_notes()
-        main()
         break
             
             
 
-
-
-   
